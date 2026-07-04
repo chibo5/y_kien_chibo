947450f8fc6c7c4b337eca29fef8264468c8380b
chibo5 <hopthu.chibo5@gmail.com>
2026-07-04 21:40:07 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -73,12 +73,12 @@ def xuat_file_word():
 # --- GIAO DIỆN WEB CẢI TIẾN (KHÔNG DÙNG FORM NHÓM) ---
 st.set_page_config(page_title="Lấy ý kiến Chi bộ", page_icon="☭", layout="centered")
 
-st.markdown("###:blue[☭ HỆ THỐNG THU THẬP Ý KIẾN]")
+st.markdown("blue[☭ HỆ THỐNG THU THẬP Ý KIẾN]")
 st.markdown('<p style="color:blue; font-size:15px;">Xin chào các đồng chí, vui lòng điền thông tin và đóng góp ý kiến xây dựng Chi bộ.</p>', unsafe_allow_html=True)
 
 
 # Các ô nhập liệu đặt độc lập
-danh_sach_mang = ["Xây dựng Đảng", Chuyên môn", "Đoàn trường", "Giáo dục thể chất", "Tin học", "Văn phòng", "Ý kiến khác"]
+danh_sach_mang = ["Xây dựng Đảng", "Chuyên môn", "Đoàn trường", "Giáo dục thể chất", "Tin học", "Văn phòng", "Ý kiến khác"]
 mang_selected = st.selectbox("1. Chọn Mảng/Bộ phận đóng góp ý kiến:", danh_sach_mang)
 
 ho_ten = st.text_input("2. Họ và tên (Có thể để trống để giữ bí mật danh tính):", key="input_hoten")
